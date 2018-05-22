af3c08326650a8717d8ceb2552848d9690615b94
Mustafa Bayer <mustafa.bayer@uyumsoft.com>
2018-05-22 09:38:47 +0300

    .

---
+++ api/UserLogin/Models/Modul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserLogin.Models
+{
+    public partial class Modul
+    {
+        public string ACIKLAMA { get; set; }
+        public DateTime CREATEDATE { get; set; }
+        public int CREATEUSER { get; set; }
+        public bool DURUM { get; set; }
+        public string FIRMAKOD { get; set; }
+        [Key]
+        public int ID { get; set; }
+        public string MODULKOD { get; set; }
+        public string MODULPATH { get; set; }
+        public DateTime UPDATEDATE { get; set; }
+        public int UPDATEUSER { get; set; }
+    }
+}
